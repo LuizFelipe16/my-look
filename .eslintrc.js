e344efbef1859ce8f56367dffe2e7eee988a0a03
Luiz Felipe <felipefelizatti215@gmail.com>
2022-08-09 04:42:15 -0300

    deploy

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,21 +5,7 @@ module.exports = {
     ecmaVersion: 2020, // enable parsing latest ECMAScript
     sourceType: 'module', // allow use of imports
   },
-  plugins: ['react', 'next', '@typescript-eslint', 'jest', ''],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:jest/recommended',
-    'prettier',
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended',
-  ],
-  rules: {
-    // here add any ESlint rules to overwrite those
-    // from the recommended extensions
-    'prettier/prettier': ['error', { endOfLine: 'on' }],
-    '@typescript-eslint/no-unused-vars': 'error',
-    '@typescript-eslint/no-explicit-any': 'error',
-    quotes: ['error', 'single'],
-  },
+  plugins: [],
+  extends: [],
+  rules: {},
 }
