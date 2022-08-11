e2c21a27e55d274a4fb26bca2b65f9120fd268ad
Luiz Felipe <felipefelizatti215@gmail.com>
2022-08-11 04:12:53 -0300

    add errors page

--- src/components/pages/index.ts
+++ src/components/pages/index.ts
@@ -2,3 +2,4 @@ export * from './Welcome';
 export * from './Shop';
 export * from './Reviews';
 export * from './AppStore';
+export * from './ErrorPage';
--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -1,5 +1,5 @@
 import { ErrorPage } from 'components/pages';
 
 export default function MyPageNotFound() {
-  return <ErrorPage title='Ops... página não encontrada' />
+  return <ErrorPage title='Ops... ocorreu um inesperado' />
 };
